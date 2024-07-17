de5cdd0287df8a5eb87a6c8bfbbcda5a9986645b
Jitendra Singh <hellojitens@gmail.com>
2024-07-17 21:54:54 +0530

    contains main code

---
+++ main.py
@@ -0,0 +1,8 @@
+from calendar import*
+year = int(input('Enter Year:'))
+print(calendar(year,3,1,8,6))
+
+#3 = 3 characters for days (Mon, Tue...)
+#1 = 1 line (row) for each week
+#8 = 8 rows for each month
+#6 = 6 columns for each month
